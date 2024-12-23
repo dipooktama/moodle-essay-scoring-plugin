dbf1a26fd4d274aac4129313475f3388c2f627c2
dipooktama <dipooktama@usu.ac.id>
2024-12-23 17:51:55 +0700

    init project

---
+++ essay_scoring/lang/en/block_essay_scoring.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_essay_scoring
+ * @category    string
+ * @copyright   2024 dipo <dipooktama@usu.ac.id>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['apiendpoint'] = 'API Endpoint';
+$string['apiendpoint_desc'] = 'Enter the URL of your scoring backend API (e.g., https://api.example.com/score)';
+$string['essay_scoring:addinstance'] = 'Add a new pluginname block';
+$string['essay_scoring:myaddinstance'] = 'Add a new pluginname block to the My dashboard';
+$string['essayscoring'] = 'Essay Scoring';
+$string['pluginname'] = 'Essay Scoring';
+$string['pluginname'] = 'Essay Scoring';
+$string['settings'] = 'Essay Scoring Settings';
+$string['viewscoring'] = 'View Essay Score';
